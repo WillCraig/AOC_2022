32b5e50906c6ccf6012ecb685bb556e959e93e8e
William Craig <wdcraig99@gmail.com>
2022-12-01 10:01:01 -0500

    Added comments and formatting.

--- day1/day1.py
+++ day1/day1.py
@@ -1,31 +1,30 @@
 
 
 if __name__ == '__main__':
-    
-    #with open('day1_test.txt') as in_file:
+
+    # with open('day1_test.txt') as in_file:
     with open('day1_given.txt') as in_file:
         data = [line.rstrip() for line in in_file]
-    
 
-    
     calc_cal = []
-    
+
     sum = 0
-    
-    
+
+    # could be improved...
     while len(data) > 0:
+        # went for a queueuueueue style solution.
         popped = data.pop(0)
         if popped != '':
             sum += int(popped)
         else:
             calc_cal.append(sum)
             sum = 0
-    
+
+    # this feels stupid too
+    del data[:]
+
+    # hate this
     calc_cal.sort(reverse=True)
-    
-    print(f"{calc_cal[0]}")
-    
-    print('\n')
-    
-    print((calc_cal[0] + calc_cal[1] + calc_cal[2]))
-    
+
+    print(f"Highest Cal Count: {calc_cal[0]}\n")
+    print(f"Sum of Top 3: {(calc_cal[0] + calc_cal[1] + calc_cal[2])}")
