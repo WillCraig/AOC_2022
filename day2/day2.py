d4956798b17dc4677b7e88302d10b921f9be9892
William Craig <wdcraig99@gmail.com>
2022-12-02 09:38:07 -0500

    formatted python document and moved to day folder

--- day2/day2.py
+++ day2/day2.py
@@ -3,7 +3,7 @@
 def play_round(ai: chr, usr: chr) -> int:
     ai_play = -1
     usr_play = -1
-    
+
     # Switch/Match statement?
     if ai == 'A':
         ai_play = 1
@@ -11,21 +11,19 @@ def play_round(ai: chr, usr: chr) -> int:
         ai_play = 2
     elif ai == 'C':
         ai_play = 3
-    
+
     if usr == 'X':
         usr_play = 1
     elif usr == 'Y':
         usr_play = 2
     elif usr == 'Z':
         usr_play = 3
-        
-        
-        
+
     # 0 for loss
     # 3 for draw
     # 6 for win
     # + move.
-        
+
     if usr_play == ai_play:
         # draw
         return usr_play + 3
@@ -46,11 +44,8 @@ def play_round(ai: chr, usr: chr) -> int:
         return -1
 
 
-
 def throw_game(a, u):
-    
-    
-    
+
     if u == 'X':
         if a == 'A':
             return 3
@@ -73,24 +68,19 @@ def throw_game(a, u):
             return 3 + 6
         else:
             return 1 + 6
-    
+
 
 if __name__ == '__main__':
 
-    
-    #with open('day2_test.txt') as in_file:
+    # with open('day2_test.txt') as in_file:
     with open('day2_given.txt') as in_file:
         data = [line.rstrip() for line in in_file]
-
 
     game = 0
     thrown = 0
     for moves in data:
         game += play_round(moves[0], moves[2])
         thrown += throw_game(moves[0], moves[2])
-        
+
     print("TOTAL SCORE", game)
     print("ULTIMATE WIN:", thrown)
-    
-
-
